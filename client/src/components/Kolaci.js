54a3987ebab260731632e1587b02c536fba333e5
stefan <stefan.milosevic@rgz.gov.rs>
2025-05-13 21:04:53 +0200

    poravnavanje prikaza za kolace

--- client/src/components/Kolaci.js
+++ client/src/components/Kolaci.js
@@ -1,75 +1,56 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
-const Kolaci = ({role})=>{
-    return(
-        <div className="row">
-            <div className="col-sm-4">
-            </div>
-                <div className="col-sm-4 ">
+
+const Kolaci = ({ role }) => {
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="w-100" style={{ maxWidth: '500px' }}>
+                <div className="mb-3 text-center">
                     <Link to='/dajKolace'>
-                        <button className='btn btn-default btn-lg  btn-block'>
+                        <button className='btn btn-default btn-lg btn-block'>
                             Izlistaj kolace
                         </button>
-                        </Link>
-                </div>
-                {role.rola_1===true   ?
-                <>
-                <div className='row'></div>
-                <br></br>
-                <br></br>
-                <div className='col-sm-4'></div>
-                <div className='col-sm-4'>
-                    <Link to='/kreirajKolac'>
-                        <button className='btn btn-default btn-lg  btn-block'>
-                            Kreiraj kolac /edit
-                        </button>
                     </Link>
+                </div>
 
-                </div>
-                
-                
-                <div className='row'></div>
-                <br></br>
-                <br></br>
-                <div className='col-sm-4'></div>
-                
-                <div className='col-sm-4'>
-                    <Link to='/kalkulacije'>
-                        <button  className='btn btn-primary btn-lg  btn-block'>
-                            Kalkulacije
-                        </button>
-                    </Link>
+                {role.rola_1 === true && (
+                    <>
+                        <div className="mb-3 text-center">
+                            <Link to='/kreirajKolac'>
+                                <button className='btn btn-default btn-lg btn-block'>
+                                    Kreiraj kolac / edit
+                                </button>
+                            </Link>
+                        </div>
 
-                </div>
-                 
-                <div className='row'></div>
-                <br></br>
-                <br></br>
-                <div className='col-sm-4'></div>
-                <div className='col-sm-4'>
-                    <Link to='/napraviRecepturu'>
-                        <button className='btn btn-default btn-lg  btn-block'>
-                            Kreiraj recepturu
-                        </button>
-                        </Link>
-                </div>
-                <div className='row'></div>
-                <br></br>
-                <br></br>
-                <div className='col-sm-4'></div>
-                <div className='col-sm-4'>
-                    <Link to='/azurirajRecepturu'>
-                        <button className='btn btn-default btn-lg  btn-block'>
-                            Azuriraj recepturu
-                        </button>
-                        </Link>
-                </div>
-                </> 
-                : null}
-                
-                <br /><br />
+                        <div className="mb-3 text-center">
+                            <Link to='/kalkulacije'>
+                                <button className='btn btn-primary btn-lg btn-block'>
+                                    Kalkulacije
+                                </button>
+                            </Link>
+                        </div>
+
+                        <div className="mb-3 text-center">
+                            <Link to='/napraviRecepturu'>
+                                <button className='btn btn-default btn-lg btn-block'>
+                                    Kreiraj recepturu
+                                </button>
+                            </Link>
+                        </div>
+
+                        <div className="mb-3 text-center">
+                            <Link to='/azurirajRecepturu'>
+                                <button className='btn btn-default btn-lg btn-block'>
+                                    Azuriraj recepturu
+                                </button>
+                            </Link>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
-    )
+    );
 };
+
 export default Kolaci;
